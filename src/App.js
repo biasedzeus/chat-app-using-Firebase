89cd1a61d587d0125ab503604a976e3f4eb74b98
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-26 20:35:08 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import './App.css';
-import HomePage from './Componets/Homepage/HomePage';
-import Login from './Componets/Login';
-import SideBar from './Componets/SideBar/SideBar';
-import TopBar from './Componets/TopBar/TopBar';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
+//Import Components
+import HomePage from "./Componets/Homepage/HomePage";
+import TopBar from "./Componets/TopBar/TopBar";
+import Login from "./Componets/Login";
+
+//REACT ROUTER IMPORTS
+import { Switch, Route } from "react-router";
+
+// Initialize Firebase
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./Config";
+
+// INitailize firbase
+import { collection, addDoc } from "firebase/firestore";
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+const userRef = collection(db, "users");
+console.log("UserCollection ===>", userRef);
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+   
+
+
+  async function addNewUser() {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        username: "",
+        userID: "",
+        
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+
+  
+
   return (
-    <div >
-      <TopBar/>
-  {/* <Login/> */}
-  <HomePage/>
-  {/* <SideBar/> */}
+    <div>
+      <TopBar />
+      {/* <button onClick={() =>addNewUser()}>Clik me</button> */}
+
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
