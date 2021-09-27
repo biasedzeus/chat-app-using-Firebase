35ad102a5edded4fc193cdd8b3b00566228d751a
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-27 23:27:54 +0530

    chatbox updated

--- src/Componets/ChatBox/ChatBox.js
+++ src/Componets/ChatBox/ChatBox.js
@@ -1,30 +1,17 @@
-import React from 'react'
-import "./ChatBox.css"
+import React from "react";
+import "./ChatBox.css";
 
 function ChatBox() {
-    return (
-        <div className="col-9  fluid-container">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-             Quo repellat impedit omnis odio! Corporis qui tenetur, ex itaque blanditiis odit harum quam, 
-            culpa adipisci officiis eligendi praesentium ipsam numquam provident.
-           
-        </div>
-    )
+  return (
+    <div className="col-9  fluid-container message-box">
+      <div className="container-left">
+        <div className="message-container">well hello There steven</div>
+      </div>
+      <div className="container-right">
+        <div className="message-container">hello There ,Mike</div>
+      </div>
+    </div>
+  );
 }
 
-export default ChatBox
+export default ChatBox;
