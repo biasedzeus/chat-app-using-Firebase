8f930be3aa68ff9697d200f68ce5d82566a13a74
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-28 21:44:20 +0530

    chat functions added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Login from "./Componets/Login";
 //REACT ROUTER IMPORTS
 import { Switch, Route } from "react-router";
 
-// Initialize Firebase
+// Initialize Firebase    
 import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
@@ -44,6 +44,34 @@ function App() {
     }
   }
 
+  function ChatRoom() {
+
+    const messagesRef = firestore.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limitToLast(25);
+  
+    const [messages] = useCollectionData(query, { idField: 'id' });
+  
+    return (<>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </main>
+  
+    </>)
+  }
+
+  
+  function ChatMessage(props) {
+    const { text, uid, photoURL } = props.message;
+  
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  
+    return (<>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} />
+        <p>{text}</p>
+      </div>
+    </>)
+  }
 
   
 
