984346ec20194957be7c8e60be31cded907d7446
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-28 21:47:04 +0530

    chatbox updated

--- src/Componets/ChatBox/ChatBox.js
+++ src/Componets/ChatBox/ChatBox.js
@@ -2,6 +2,24 @@ import React from "react";
 import "./ChatBox.css";
 
 function ChatBox() {
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    
+  }
   return (
     <div className="col-9  fluid-container message-box">
       <div className="container-left">
